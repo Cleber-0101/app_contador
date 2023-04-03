3412b6c2dab329e64b96d8c3bb549036cbfc041a
Cleber-0101 <cleberbatista99@hotmail.com>
2023-04-03 19:34:00 -0300

    Organizando codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(home: Container(color: Colors.black),
-     child),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ola mundo"),
+      ),
+      drawer: Drawer(),
+    );
+  }
 }
